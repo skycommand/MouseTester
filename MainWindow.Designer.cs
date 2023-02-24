3385c4f344eec9b3f54327939fe4d47ad14b81c0
skycommand <17097175+skycommand@users.noreply.github.com>
2023-02-24 07:01:45 +0330

    Add project files.

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MouseTester
+{
+  partial class MainWindow
+  {
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      logLabel = new System.Windows.Forms.Label();
+      SuspendLayout();
+      // 
+      // logLabel
+      // 
+      logLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+      logLabel.Location = new System.Drawing.Point(0, 0);
+      logLabel.Name = "logLabel";
+      logLabel.Size = new System.Drawing.Size(800, 450);
+      logLabel.TabIndex = 0;
+      logLabel.Text = "Awaiting input";
+      logLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // MainWindow
+      // 
+      AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      ClientSize = new System.Drawing.Size(800, 450);
+      Controls.Add(this.logLabel);
+      FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      MaximizeBox = false;
+      Name = "MainWindow";
+      Text = "Mouse Tester";
+      ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Label logLabel;
+  }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+
+namespace MouseTester
+{
+  public partial class MainWindow : Form
+  {
+    private const string StrWheelDown = "↡";
+    private const string StrWheelUp = "↟";
+    private const string StrWheelDeltaZero = "⛔"; //User must never see this
+    private const string StrMouseWheel = "Mouse wheel";
+
+    private bool MouseWheelIsInUse = false;
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      /* Visual Studio 2022 form designer neither supports the OnMouseWheel event nor assigning multiple handlers to one
+       * event. Hence, we assign our handlers here. */
+      MouseWheel += new MouseEventHandler(MainWindow_AllMouseEventsInitial);
+      MouseWheel += new MouseEventHandler(MainWindow_MouseWheel);
+      MouseDown += new MouseEventHandler(MainWindow_AllMouseEventsInitial);
+      MouseDown += new MouseEventHandler(MainWindow_MouseDown);
+      logLabel.MouseDown += new MouseEventHandler(MainWindow_AllMouseEventsInitial);
+      logLabel.MouseDown += new MouseEventHandler(MainWindow_MouseDown);
+    }
+
+    private void MainWindow_AllMouseEventsInitial (object? sender, MouseEventArgs e)
+    {
+      logLabel.Text = "";
+      MouseWheel -= MainWindow_AllMouseEventsInitial;
+      MouseDown -= MainWindow_AllMouseEventsInitial;
+      logLabel.MouseDown -= MainWindow_AllMouseEventsInitial;
+    }
+
+    private void MainWindow_MouseDown(object? sender, MouseEventArgs e)
+    {
+      MouseWheelIsInUse = false;
+      logLabel.Text += " " + e.Button.ToString();
+    }
+
+    private void MainWindow_MouseWheel(object? sender, MouseEventArgs e)
+    {
+      if (!MouseWheelIsInUse) { 
+        MouseWheelIsInUse = true;
+        logLabel.Text += " " + StrMouseWheel + " ";
+      }
+      logLabel.Text += e.Delta switch
+      {
+        < 0 => StrWheelDown,
+        > 0 => StrWheelUp,
+        0 => StrWheelDeltaZero // This never runs
+      };
+    }
+  }
+}
